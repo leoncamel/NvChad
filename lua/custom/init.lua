240ffb96690d1b9cf03b3b24ad6c76b6e147bc56
Xiaolin Zhang <leoncamel@gmail.com>
2024-09-08 15:11:31 +0800

    Initial commit for leoncamel based on NvChard v2.0

---
+++ lua/custom/init.lua
@@ -0,0 +1,29 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+
+
+--
+-- From: https://nvchad.com/docs/recipes#restore_cursor_position
+--
+local autocmd = vim.api.nvim_create_autocmd
+
+autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    local line = vim.fn.line "'\""
+    if
+      line > 1
+      and line <= vim.fn.line "$"
+      and vim.bo.filetype ~= "commit"
+      and vim.fn.index({ "xxd", "gitrebase" }, vim.bo.filetype) == -1
+    then
+      vim.cmd 'normal! g`"'
+    end
+  end,
+})
+
